94482d5b363aa8a64972fb15c1835c25be19ef8e
Alaisami Mirna <maa@novatec-gmbh.de>
2019-03-29 16:01:42 +0100

    first commit

---
+++ first-push-app/src/main/java/org/cloudfoundry/training/AppInfo.java
@@ -0,0 +1,48 @@
+package org.cloudfoundry.training;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AppInfo {
+
+	public static final String MYSQL = "MySQL";
+	public static final String H2 = "H2";
+	
+	private String providerName;
+	private String appName;
+	private int instanceIndex;
+	private String spaceName;
+	private String database;
+	
+	public AppInfo() {}
+
+	public AppInfo(String providerName, String appName, int instanceIndex, String spaceName, String database) {
+		this.providerName = providerName;
+		this.appName = appName;
+		this.instanceIndex = instanceIndex;
+		this.spaceName = spaceName;
+		this.database = database;
+	}
+
+	public String getProviderName() {
+		return providerName;
+	}
+
+	public String getAppName() {
+		return appName;
+	}
+
+	public int getInstanceIndex() {
+		return instanceIndex;
+	}
+
+	public String getSpaceName() {
+		return spaceName;
+	}
+
+	public String getDatabase() {
+		return database;
+	}
+	
+	
+}
